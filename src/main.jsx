5b79213eccc092993b8274b6f8e535b746681e70
sumonaung23 <sumonaung575@gmail.com>
2023-12-31 22:24:38 +0630

    imported app from firebaseConfig file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.css'
 import { router } from './Routes/index.jsx';
+import { app } from './firebaseConfig';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

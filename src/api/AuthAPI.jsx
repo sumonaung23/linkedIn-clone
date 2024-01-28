736253326f01148d46e70c675751977ebd39207e
sumonaung23 <sumonaung575@gmail.com>
2024-01-28 21:28:45 +0630

    added method from auth and created RegisterAPI function

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,13 +1,20 @@
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebaseConfig';
 
-const LoginAPI = (email, password) => {
+export const LoginAPI = (email, password) => {
     try {
         let response = signInWithEmailAndPassword(auth, email, password);
         return response;
     } catch(err) {
-        alert(err.errors.message);
+        return err;
     }
 }
 
-export default LoginAPI
+export const RegisterAPI = (email, password) => {
+    try {
+        let response = createUserWithEmailAndPassword(auth, email, password);
+        return response;
+    } catch(err) {
+        return err;
+    }
+}

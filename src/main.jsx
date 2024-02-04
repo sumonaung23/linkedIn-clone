5826f19ba9b26b7c8b86c70eefa35d2a76932ac6
sumonaung23 <sumonaung575@gmail.com>
2024-02-04 22:48:46 +0630

    imported ToastContainer from react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.css'
 import { router } from './Routes/index.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { app } from './firebaseConfig';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>,
 )

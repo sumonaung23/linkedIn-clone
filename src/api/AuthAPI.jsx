a5ffdc0b30211cc409bb88a6ae3e70a390c0373c
sumonaung23 <sumonaung575@gmail.com>
2024-02-05 23:35:24 +0630

    created googleSignInAPI component

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,4 +1,8 @@
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import { 
+    signInWithEmailAndPassword,                createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
+} from 'firebase/auth'
 import { auth } from '../firebaseConfig';
 
 export const LoginAPI = (email, password) => {
@@ -14,6 +18,16 @@ export const RegisterAPI = (email, password) => {
     try {
         let response = createUserWithEmailAndPassword(auth, email, password);
         return response;
+    } catch(err) {
+        return err;
+    }
+}
+
+export const GoogleSignInAPI = (email, password) => {
+    try {
+        let googleProvider = new GoogleAuthProvider();
+        let res = signInWithPopup(googleProvider);
+        return res;
     } catch(err) {
         return err;
     }

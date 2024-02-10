7a9ced19db670c37ca26abc381721b6426eeafe1
sumonaung23 <sumonaung575@gmail.com>
2024-02-10 23:25:30 +0630

    added auth and useNevigate, then created Login component with useEffect

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,19 @@
 import LoginComponent from '../components/LoginComponent'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, res => {
+            if(!res?.accessToken){
+                navigate('/')
+            }
+        } )
+    }, []);
+
     return (
         <div className='login'>
             <LoginComponent />
